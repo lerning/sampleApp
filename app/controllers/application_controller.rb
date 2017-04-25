a30a8c6e37cc5d49067fa9876e2b6884fa7c3318
Joshua Lerner <joshualerner@Joshuas-MacBook-Pro.local>
2017-04-25 17:24:53 -0600

    fixing heroku deployment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
+
   def hello
-    render html: "hello, world!"
+    render html: "hello, me!"
   end
 
 end
